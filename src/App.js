b21fa5ac4ba4e355ccf49ff29140172d3a321118
Jay <jaypatel01122000@gmail.com>
2023-05-23 19:14:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import Training from './pages/Training';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/pricing' element={<Pricing/>} />
+      <Route path='/training' element={<Training/>} />
+      <Route path='/contact' element={<Contact/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
